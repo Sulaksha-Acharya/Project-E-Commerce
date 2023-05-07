8c6dc618b500d365c98c80abc52e7afe78a8b8b3
deepakspk <deepaksapkota1991@gmail.com>
2023-05-07 17:34:53 +0545

    contact form submitted, message

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,23 +1,47 @@
-import React from "react";
-import { Container } from "@mui/system";
-import contactus from "../asset/contactus.jpg";
-import ContactForm from "../component/ContactForm";
+import React from 'react'
+import { Container } from '@mui/system'
+import contactus from '../asset/contactus.jpg'
+import ContactForm from '../component/ContactForm'
+import { Grid, Typography } from '@mui/material'
 
 const Contact = () => {
   return (
     <div>
-      <Container>
+      <Container className='mt-5'>
         <img
-          className="contactus"
+          className='contactus'
           src={contactus}
-          alt="img"
-          width="100%"
+          alt='img'
+          width='100%'
           height={300}
         />
-      </Container>
-      <ContactForm />
-    </div>
-  );
-};
 
-export default Contact;
+        <Grid container spacing={2} className='mt-5'>
+          <Grid item xs={6}>
+            <Typography
+              style={{ fontSize: 28, marginBottom: 20, marginLeft: '20px' }}
+            >
+              Hi, How can we help you?
+            </Typography>
+            <Typography style={{ marginLeft: '20px' }}>
+              Send your message, we will get back to yo as soon as possible.
+            </Typography>
+            <ContactForm />
+          </Grid>
+          <Grid item xs={6}>
+            <iframe
+              frameborder='0'
+              scrolling='no'
+              height='530'
+              width='100%'
+              src='https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14130.948231973627!2d85.34462205!3d27.694520599999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snp!4v1681814921257!5m2!1sen!2snp'
+              title='Nepal Can Move kathmandu'
+            ></iframe>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  )
+}
+
+export default Contact
--- src/component/ContactForm.js
+++ src/component/ContactForm.js
@@ -1,135 +1,98 @@
-import React from "react";
-
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import TextField from "@mui/material/TextField";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Button from "@mui/material/Button";
-import { Card } from "react-bootstrap";
-import { Container } from "@mui/system";
-import { Typography } from "@mui/material";
-
-// const theme = createTheme({
-//   palette: {
-//     neutral: {
-//       main: "crimson",
-//       contrastText: "#fff",
-//     },
-//   },
-// });
+import React, { useState } from 'react'
+import { TextField, Button, Grid } from '@mui/material'
+import { Container } from '@mui/system'
+import { toast } from 'react-toastify'
 
 const Forms = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+  })
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData)
+    setFormData({
+      name: '',
+      email: '',
+      phone: '',
+      message: '',
+    })
+    toast.success('Form Submitted Successfully')
+  }
   return (
     <div>
       <div
         style={{
-          display: "flex",
-          justifyContent: "center",
+          display: 'flex',
+          justifyContent: 'center',
           marginTop: 35,
-          // backgroundColor: "#f5f5f5",
         }}
       >
         <Container>
-          <div>
-            <Typography style={{ fontSize: 28, marginBottom: 20 }}>
-              Hi, How can we help you?
-            </Typography>
-          </div>
-          <Row>
-            <Col
-              sm={12}
-              lg={6}
-              style={{
-                height: "auto",
-                position: "relative",
-                borderRadius: 10,
-                backgroundColor: "white",
-                boxShadow: "inherit",
-              }}
-            >
-              <Card>
-                <div
-                  style={{
-                    margin: 30,
-                    marginBottom: 8,
-                    backgroundColor: "white",
-                    minwidth: 400,
-                  }}
-                >
-                  <TextField fullWidth label="Enter your name" id="fullWidth" />
-                </div>
-                <div
-                  style={{
-                    margin: 30,
-                    marginBottom: 8,
-                    backgroundColor: "white",
-                    minwidth: 400,
-                  }}
-                >
-                  <TextField
-                    fullWidth
-                    label="Enter your email"
-                    id="fullWidth"
-                  />
-                </div>
-                <div
-                  style={{
-                    margin: 30,
-                    marginBottom: 8,
-                    backgroundColor: "white",
-                    minwidth: 400,
-                  }}
-                >
-                  <TextField
-                    fullWidth
-                    label="Enter your phone number"
-                    id="fullWidth"
-                  />
-                </div>
-                <div
-                  style={{
-                    margin: 30,
-                    marginBottom: 30,
-                    backgroundColor: "white",
-                    minwidth: 400,
-                  }}
-                >
-                  <TextField
-                    fullWidth
-                    id="outlined-multiline-static"
-                    label="Enter your message"
-                    multiline
-                    rows={4}
-                  />
-                </div>
-
-                {/* <ThemeProvider theme={theme}> */}
-                <Button
-                  color="primary"
-                  style={{ marginLeft: 30, marginBottom: 20, width: 200 }}
-                  size="large"
-                  variant="contained"
-                >
+          <form onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  label='Name'
+                  name='name'
+                  value={formData.name}
+                  onChange={handleChange}
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  type='email'
+                  label='Email'
+                  name='email'
+                  value={formData.email}
+                  onChange={handleChange}
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label='Phone Number'
+                  type='number'
+                  name='phone'
+                  value={formData.phone}
+                  onChange={handleChange}
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label='Message'
+                  name='message'
+                  value={formData.message}
+                  onChange={handleChange}
+                  fullWidth
+                  multiline
+                  rows={5}
+                  required
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button type='submit' variant='contained' color='primary'>
                   Submit
                 </Button>
-                {/* </ThemeProvider> */}
-              </Card>
-            </Col>
-            <Col lg={6} sm={12}>
-              <iframe
-                frameborder="0"
-                scrolling="no"
-                height="530"
-                width="100%"
-                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14130.948231973627!2d85.34462205!3d27.694520599999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snp!4v1681814921257!5m2!1sen!2snp"
-                title="Nepal Can Move kathmandu"
-              ></iframe>
-            </Col>
-          </Row>
+              </Grid>
+            </Grid>
+          </form>
         </Container>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Forms;
+export default Forms
