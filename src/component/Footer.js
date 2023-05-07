59aadeb6f39b75e0fc5a74300b9c6ed6dabebc2b
deepakspk <deepaksapkota1991@gmail.com>
2023-05-07 21:22:53 +0545

    footer added

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,99 +1,138 @@
-import { Typography } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
-import { Container } from '@mui/system'
+import Container from 'react-bootstrap/Container'
 import logo from '../asset/logo.png'
 import { blue } from '@mui/material/colors'
 import { FaInstagram, FaFacebook, FaLinkedin, FaYoutube } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const Footer = () => (
-  <div
-    style={{
-      // backgroundColor: "dodgerblue",
-      backgroundColor: 'rgba(0, 82, 93, 0.2)',
-      display: 'flex',
-      marginTop: 50,
-    }}
-  >
-    <Container>
-      <Row style={{ padding: 20 }}>
-        <Typography>
-          <Typography variant='h6'>
-            {' '}
-            Experience Hassle-Free Online Shopping with ShopCart
-          </Typography>
-          <Typography color='text.secondary'>
-            E-commerce has evolved over the past few years and since it’s easier
-            and more convenient, it is evident that customers are actually
-            switching to the trend of online shopping. shopping store, brings a
-            whole new concept by showcasing a number of famous brands under one
-            roof. Not only does it fulfill clothing necessities of both men and
-            women but you can also shop for all kinds of appliances like air
-            conditioners, heaters, refrigerators, LED TVs and a lot more. Simply
-            select your favorite brand like Samsung, Apple, HP, Huawei, Dell,
-            Canon, Nikon, etc and get yourself the best electronic items.
-          </Typography>
-        </Typography>
-      </Row>
-    </Container>
-    <Container>
-      <Row style={{ padding: 20 }}>
-        <Typography style={{ marginRight: 5 }}>
-          <h5>Top Categories & Brands</h5>
-          <h6> New Mobile Phones </h6>
-          <Typography color='text.secondary'>
-            {' '}
-            Samsung Mobile Phones, Xiaomi Mobiles, Nokia Mobiles,Oppo
-            Mobiles,Apple iPhones,OnePlus,Sony Mobiles,Huawei Mobiles,Lenovo
-            Mobiles.
-            <b>
-              <Typography variant='h6'>Latest Laptops</Typography>
-            </b>
-            <Typography>
-              Apple Laptop, Samsung Laptops, Dell Laptops, HP Laptops, Acer
-              Laptops etc
+  <>
+    <div
+      style={{
+        backgroundColor: 'rgba(0, 82, 93, 0.2)',
+        display: 'flex',
+        marginTop: 50,
+      }}
+    >
+      <Container>
+        <Grid container spacing={2} className='py-5'>
+          <Grid item xs={12} md={4}>
+            <Typography variant='h6'>
+              Experience Hassle-Free Online Shopping with ShopCart
             </Typography>
-            <h6> LRD TVS Prices</h6>
-            <Typography>
-              {' '}
-              Samsung LED TVs,Sony LED TVs, Videcon LED TVs
+            <Typography color='text.secondary'>
+              E-commerce has evolved over the past few years and since it’s
+              easier and more convenient, it is evident that customers are
+              actually switching to the trend of online shopping. shopping
+              store, brings a whole new concept by showcasing a number of famous
+              brands under one roof.
             </Typography>
-          </Typography>
-        </Typography>
-      </Row>
-    </Container>
-    <Container>
-      <Row style={{ padding: 20 }}>
-        <Typography>
-          <h5> 100% ORIGINAL</h5>
-          <p color='text.seconczry'>
-            {' '}
-            Guarantee for all product at shopcart.com
-          </p>
-          <h5> Return within 30days</h5>
-          <p>of receiving your order</p>
-          <Typography style={{ marginRight: 40 }}>
-            <img
-              className='logo'
-              src={logo}
-              alt='img'
-              width={150}
-              height={35}
-              style={{ marginRight: 30 }}
-            />
-            <b> Happy Shopping!</b>
-            <Typography style={{ marginTop: 10 }}> Follow Us!</Typography>
-            <div style={{ gap: '10' }}>
-              <FaInstagram />
-              <FaFacebook />
-              <FaLinkedin />
-              <FaYoutube />
-            </div>
-          </Typography>
-        </Typography>
-      </Row>
-    </Container>
-  </div>
+            <Typography style={{ marginRight: 40, marginTop: 20 }}>
+              <Link to='/'>
+                <img
+                  className='logo'
+                  src={logo}
+                  alt='img'
+                  width={150}
+                  height={35}
+                  style={{ marginRight: 30 }}
+                />
+              </Link>
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={1}></Grid>
+          <Grid item xs={12} md={3}>
+            <Typography style={{ marginRight: 5 }}>
+              <h5>Top Categories</h5>
+              <li> MOBILE PHONES </li>
+              <li> CAMERAS</li>
+              <li> LATEST GADGETS</li>
+              <li> COMPUTER COMPONENTS</li>
+              <li> WOMEN'S FASHION</li>
+              <li> MEN'S FASHION</li>
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={1}></Grid>
+          <Grid item xs={12} md={3}>
+            <Typography>
+              <h5> 100% ORIGINAL</h5>
+              <p color='text.secondary'>
+                Guarantee for all product at shopcart.com
+              </p>
+              <h5> Return within 30days</h5>
+              <p>of receiving your order</p>
+              <Typography variant='h6' style={{ marginTop: 10 }}>
+                Follow Us!
+              </Typography>
+              <div style={{ gap: '10' }}>
+                <a
+                  href='https://www.instagram.com'
+                  rel='noreferrer'
+                  target='_blank'
+                  className='SocialMediaIcons'
+                >
+                  <FaInstagram
+                    style={{ color: 'rgb(244, 67, 54)' }}
+                    className='me-2'
+                    fontSize='25px'
+                  />
+                </a>
+                <a
+                  href='https://www.facebook.com'
+                  rel='noreferrer'
+                  target='_blank'
+                  className='SocialMediaIcons'
+                >
+                  <FaFacebook
+                    style={{ color: 'rgb(244, 67, 54)' }}
+                    className='me-2'
+                    fontSize='25px'
+                  />
+                </a>
+                <a
+                  href='https://www.linkedin.com/'
+                  rel='noreferrer'
+                  target='_blank'
+                  className='SocialMediaIcons'
+                >
+                  <FaLinkedin
+                    style={{ color: 'rgb(244, 67, 54)' }}
+                    className='me-2'
+                    fontSize='25px'
+                  />
+                </a>
+                <a
+                  href='https://www.youtube.com/'
+                  rel='noreferrer'
+                  target='_blank'
+                  className='SocialMediaIcons'
+                >
+                  <FaYoutube
+                    style={{ color: 'rgb(244, 67, 54)' }}
+                    className='me-2'
+                    fontSize='25px'
+                  />
+                </a>
+              </div>
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+    <div
+      style={{
+        backgroundColor: 'rgb(244, 67, 54)',
+        padding: '10px',
+      }}
+      className='text-center'
+    >
+      <h6 style={{ textAlign: 'center' }}>
+        © 2023 ShopCart - All Right Reserve
+      </h6>
+    </div>
+  </>
 )
 
 export default Footer
