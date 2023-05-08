80ce00b23ff88d399a642b3036bea36c2161be9d
Sulaksha-Acharya <suprinaacharya9876@gmail.com>
2023-05-08 14:47:14 +0545

    wishlist changes

--- src/Pages/Wishlist.js
+++ src/Pages/Wishlist.js
@@ -1,70 +1,70 @@
 // import * as React from "react";
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableContainer from '@mui/material/TableContainer'
-import TableHead from '@mui/material/TableHead'
-import TableRow from '@mui/material/TableRow'
-import Paper from '@mui/material/Paper'
-import { Container, margin } from '@mui/system'
-import ItemCard from '../component/ItemCard'
-import { useDispatch, useSelector } from 'react-redux'
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Container, margin } from "@mui/system";
+import ItemCard from "../component/ItemCard";
+import { useDispatch, useSelector } from "react-redux";
 import {
   cartActions,
   selectItemlist,
   wishlistItemlist,
-} from '../store/cart-slice'
-import React, { useState } from 'react'
-import Checkbox from '@mui/material/Checkbox'
+} from "../store/cart-slice";
+import React, { useState } from "react";
+import Checkbox from "@mui/material/Checkbox";
 // import { FiDelete } from "react-icons/Fi";
-import { CiTrash } from 'react-icons/ci'
-import { auth } from '../config/firebase'
-import { useNavigate } from 'react-router-dom'
-import { FcApproval } from 'react-icons/fc'
-import notFound from '../asset/notFound.png'
-import Card from '@mui/material/Card'
+import { CiTrash } from "react-icons/ci";
+import { auth } from "../config/firebase";
+import { useNavigate } from "react-router-dom";
+import { FcApproval } from "react-icons/fc";
+import notFound from "../asset/notFound.png";
+import Card from "@mui/material/Card";
 
-const rows = []
+const rows = [];
 const Wishlist = ({ products, total, onCheckoutClicked }) => {
-  const wishlistItems = useSelector(wishlistItemlist)
-  const [quantity, setQuantity] = useState(0)
+  const wishlistItems = useSelector(wishlistItemlist);
+  const [quantity, setQuantity] = useState(0);
   const Wishlist = ({ product }) => {
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
     dispatch(
       cartActions.Wishlist({
         ...product,
       })
-    )
-  }
+    );
+  };
 
   return (
     <div>
-      <Container className='mb-5' style={{ minHeight: '50vh' }}>
-        <h2 style={{ textAlign: 'center', marginTop: '20px' }}>
+      <Container className="mb-5" style={{ minHeight: "50vh" }}>
+        <h2 style={{ textAlign: "center", marginTop: "20px" }}>
           Your Wishlist
         </h2>
         <br></br>
         {wishlistItems && wishlistItems.length > 0 ? (
-          <Card style={{ padding: 10, width: '100%' }}>
+          <Card style={{ padding: 10, width: "100%" }}>
             <Table
               sx={{ minWidth: 650 }}
-              size='large'
-              aria-label='a dense table'
+              size="large"
+              aria-label="a dense table"
             >
               <TableHead>
                 <TableRow>
                   {/* <CiTrash /> */}
                   <TableCell></TableCell>
-                  <TableCell style={{ marginLeft: '160', fontSize: 20 }}>
+                  <TableCell style={{ marginLeft: "160", fontSize: 20 }}>
                     Product name
                   </TableCell>
-                  <TableCell style={{ fontSize: 20 }} align='left'>
+                  <TableCell style={{ fontSize: 20 }} align="left">
                     Unit price
                   </TableCell>
                   <TableCell
-                    style={{ marginLeft: '100', fontSize: 20 }}
-                    align='left'
+                    style={{ marginLeft: "100", fontSize: 20 }}
+                    align="left"
                   >
                     Stock Status
                   </TableCell>
@@ -74,31 +74,31 @@ const Wishlist = ({ products, total, onCheckoutClicked }) => {
                 {wishlistItems?.map((product) => (
                   <TableRow
                     key={product.title}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
                   >
                     <TableCell>
                       <CiTrash />
                       {/* <Checkbox defaultChecked /> */}
                     </TableCell>
 
-                    <TableCell component='th' scope='row'>
+                    <TableCell component="th" scope="row">
                       <img
                         style={{
-                          width: '40px',
-                          height: '40px',
-                          objectFit: 'cover',
-                          marginRight: '1rem',
+                          width: "40px",
+                          height: "40px",
+                          objectFit: "cover",
+                          marginRight: "1rem",
                         }}
                         src={product.image}
-                        alt='productImage'
+                        alt="productImage"
                       />
                       {product.title}
                       {/* <FiDelete /> */}
                     </TableCell>
-                    <TableCell component='th' scope='row'>
+                    <TableCell component="th" scope="row">
                       {product.price}
                     </TableCell>
-                    <TableCell component='th' scope='row'>
+                    <TableCell component="th" scope="row">
                       {product.status}
                       <FcApproval />
                     </TableCell>
@@ -109,23 +109,23 @@ const Wishlist = ({ products, total, onCheckoutClicked }) => {
           </Card>
         ) : (
           <>
-            <Card style={{ padding: 10, width: '100%' }}>
+            <Card style={{ padding: 10, width: "100%" }}>
               <div
                 style={{
-                  textAlign: 'center',
-                  marginTop: '40px',
-                  marginBottom: '40px',
+                  textAlign: "center",
+                  marginTop: "40px",
+                  marginBottom: "40px",
                 }}
               >
                 <img
                   src={notFound}
-                  alt='not found'
-                  loading='lazy'
-                  width='30%'
-                  textAlign='center'
-                  alignItems='center'
+                  alt="not found"
+                  loading="lazy"
+                  width="30%"
+                  textAlign="center"
+                  alignItems="center"
                 />
-                <h5 style={{ textAlign: 'center' }}>
+                <h5 style={{ textAlign: "center" }}>
                   No Product in your wishlist.
                 </h5>
               </div>
@@ -134,7 +134,7 @@ const Wishlist = ({ products, total, onCheckoutClicked }) => {
         )}
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Wishlist
+export default Wishlist;
